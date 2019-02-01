17a1a0849f1fc1f41fb7251b8e5d3333e5f04d17
Dardanboy <dardan.iljazi@gmail.com>
2019-02-01 13:49:03 +0100

    Addd In and Out Button only when possible

--- resources/views/usbActionButtons/inputTest.blade.php
+++ resources/views/usbActionButtons/inputTest.blade.php
@@ -1,4 +1,11 @@
-<form method="post" action="usbs/in/{{ $actualUsb->id }}">
-    @csrf
-    <button type="submit" class="btn btn-info">IN</button>
-</form>
+@switch($key)
+    @case('used')
+    @case('absent')
+    @case('pulled-not-initialized')
+    <form method="post" action="usbs/in/{{ $actualUsb->id }}">
+        @csrf
+        <button type="submit" class="btn btn-info">IN</button>
+    </form>
+    @break
+@endswitch
+
